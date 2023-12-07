15b5a9a8120bc3e624334ea0e6f9f2737f6b6bd3
Mehdi Abjadpour <m.abjadpour@bechstein-digital.com>
2023-12-07 16:04:18 +0100

    Reveal password for verification.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const passwordField = document.querySelector('#password');
 togglePassword.addEventListener('click', function (e) {
     const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordField.setAttribute('type', type);
+    this.classList.toggle('fa-eye');
     this.classList.toggle('fa-eye-slash');
 });
 
